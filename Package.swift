5e17d17fdc8c20d659f90e00e2d39ed7bf965d26
Joseph Montanez <sutabi@gmail.com>
2023-08-17 12:16:52 -0700

    Update Package.swift
    use own smoke-http repo

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "0.1.3"),
-        .package(url: "https://github.com/amzn/smoke-http.git", from: "1.0.0"),
+        .package(url: "https://github.com/joseph-montanez/smoke-http.git", .branch("ios-9")),
     ],
     targets: [
         .target(
